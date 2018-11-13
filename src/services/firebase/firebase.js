b45c22ded06d51e8860b377c4b52827b8db0c8a4
Karl Hofer <kh@viewar.com>
2018-11-13 17:04:40 +0100

    Reenable production/development databases

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -4,8 +4,7 @@ import 'firebase/database';
 
 import { CONFIG_DEV, CONFIG_PROD } from './constants';
 
-// const config = process.env.NODE_ENV === 'production' ? CONFIG_PROD : CONFIG_DEV;
-const config = CONFIG_PROD;
+const config = process.env.NODE_ENV === 'production' ? CONFIG_PROD : CONFIG_DEV;
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
